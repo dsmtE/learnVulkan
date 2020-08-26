3d5c0cfa801f52bf29a2c9b6129d28bf729c7807
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2020-08-27 00:11:48 +0200

    add main useful infos

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,50 @@
 
 namespace {
 
+	void PrintVulkanSdkInformation() {
+		std::cout << "Vulkan SDK Header Version: " << VK_HEADER_VERSION << std::endl;
+	}
+
+	void PrintVulkanInstanceInformation(const Vulkan::Application& application) {
+
+		std::cout << "Vulkan Instance Extensions: " << std::endl;
+
+		for (const auto& extension : application.Extensions()) {
+			std::cout << "- " << extension.extensionName << " (" << Vulkan::Version(extension.specVersion) << ")" << std::endl;
+		}
+	}
+
+	void PrintVulkanDevices(const Vulkan::Application& application) {
+		std::cout << "Vulkan Devices: " << std::endl;
+
+		for (const auto& device : application.PhysicalDevices()) {
+			VkPhysicalDeviceProperties prop;
+			vkGetPhysicalDeviceProperties(device, &prop);
+
+			VkPhysicalDeviceFeatures features;
+			vkGetPhysicalDeviceFeatures(device, &features);
+
+			const Vulkan::Version vulkanVersion(prop.apiVersion);
+			const Vulkan::Version driverVersion(prop.driverVersion, prop.vendorID);
+
+			std::cout << "- [" << prop.deviceID << "] ";
+			std::cout << Vulkan::toString(prop.vendorID) << " '" << prop.deviceName;
+			std::cout << "' (";
+			std::cout << prop.deviceType << ": ";
+			std::cout << "vulkan " << vulkanVersion << ", ";
+			std::cout << "driver " << driverVersion;
+			std::cout << ")" << std::endl;
+		}
+	}
+
+	void PrintVulkanSwapChainInformation(const Vulkan::Application& application) {
+		const Vulkan::SwapChain& swapChain = application.SwapChain();
+
+		std::cout << "Swap Chain: " << std::endl;
+		std::cout << "- image count: " << swapChain.Images().size() << std::endl;
+		std::cout << "- present mode: " << Vulkan::toString(swapChain.PresentMode()) << std::endl;
+	}
+
 	void SetVulkanDevice(Vulkan::Application& application) {
 		const auto& physicalDevices = application.PhysicalDevices();
 		const auto result = std::find_if(physicalDevices.begin(), physicalDevices.end(), [](const VkPhysicalDevice& device)
@@ -57,7 +101,11 @@ int main(int argc, const char* argv[]) noexcept {
 
 		TriangleApp application(windowConfig);
 
+		PrintVulkanSdkInformation();
+		PrintVulkanInstanceInformation(application);
+		PrintVulkanDevices(application);
 		SetVulkanDevice(application);
+		PrintVulkanSwapChainInformation(application);
 
 		application.Run();
 
@@ -83,4 +131,3 @@ int main(int argc, const char* argv[]) noexcept {
 
 	return EXIT_FAILURE;
 }
-
